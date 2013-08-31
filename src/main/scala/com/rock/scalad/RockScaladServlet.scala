4417737059975d573f9cdcad8589c6eddf14f434
Skylar Lowery <skylar.lowery21@gmail.com>
2013-08-31 00:16:58 -0600

    ENDPOINT: Added awesome endpoint
    Passing in your name and awesomeness to /awesome/:user will tell you how awesome you are based on the awesome score you also send in. Yeah, awesome

--- src/main/scala/com/rock/scalad/RockScaladServlet.scala
+++ src/main/scala/com/rock/scalad/RockScaladServlet.scala
@@ -7,12 +7,26 @@ class RockScaladServlet extends RockScaladStack {
 
   get("/") {
     <html>
-      <body>
-        <h1>Welcome to Rock Scalad!</h1>
+		  <body>
+    		<h1>Welcome to Rock Scalad!</h1>
         <h3>Nothing you wouldn't show your own mother so far, but this is only the beginning.</h3>
         <p>Please don't <a href="no-way-jose">click random things</a>.</p>
       </body>
     </html>
+  }
+
+  get("/awesome/:user") {
+  	// If they don't include awesomeness, they're clearly not awesome
+  	val awesomeness:Int = params.getOrElse("awesomeness","0").toInt
+
+  	// If they don't send a user, they'll find themselves in no man's land (404)
+  	val user:String = params("user")
+
+  	<html>
+  		<body>
+  			<p>Hello {user}, your awesomeness is looking to be about {awesomeness}</p>
+  		</body>
+		</html>
   }
 
   notFound {
