1405d649f0db38b321f0a73f25d6e39c6b0cc369
Skylar Lowery <skylar.lowery21@gmail.com>
2013-08-30 23:41:02 -0600

    INIT: Added 404. Removed hello world code
    Basic clean up the template app. Added some comical basic info to the main page and implemented notFound for unmatched routes.

--- src/main/scala/com/rock/scalad/RockScaladServlet.scala
+++ src/main/scala/com/rock/scalad/RockScaladServlet.scala
@@ -8,10 +8,20 @@ class RockScaladServlet extends RockScaladStack {
   get("/") {
     <html>
       <body>
-        <h1>Hello, world!</h1>
-        Say <a href="hello-scalate">hello to Scalate</a>.
+        <h1>Welcome to Rock Scalad!</h1>
+        <h3>Nothing you wouldn't show your own mother so far, but this is only the beginning.</h3>
+        <p>Please don't <a href="no-way-jose">click random things</a>.</p>
       </body>
     </html>
+  }
+
+  notFound {
+  	<html>
+  		<body>
+  			<h1>Woah, there...</h1>
+  			<p>The page you request was not found. <a href="/">Go home surfer, you're drunk</a></p>
+  		</body>
+  	</html>
   }
   
 }
